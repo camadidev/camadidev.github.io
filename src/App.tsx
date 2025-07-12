daa47e751ae78df56204aa9ebc70ff1fe0620a15
camadidev <camadidev@gmail.com>
2025-07-11 19:24:04 -0600

    commit: cambio de logo y links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 
-import reactLogo from './assets/react.svg'
 import logoCamadi from './assets/logoCamadi.png'
 import './App.css'
 
@@ -9,17 +8,37 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://camadidev.github.io" target="_blank">
           <img src={logoCamadi} className="logo" alt="Logo CAMADI" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
       </div>
       <h1>CAMADI - En desarrollo</h1>
+
+      <h2>APPS</h2>
       <div className="card">
-       
+        <a 
+        href="https://play.google.com/store/apps/details?id=com.camadi.fortunenumerosuerte" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline"
+      >
+        Fortuna Numeros de la suerte
+      </a>
+
       </div>
+
+      <div className="card">
+        <a 
+        href="https://play.google.com/store/apps/details?id=com.camadi.rifitas" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline"
+      >
+        Rifitas
+      </a>
+      </div>
+
       <p className="read-the-docs">
         
       </p>
